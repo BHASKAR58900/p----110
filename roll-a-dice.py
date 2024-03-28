47a63fd652e68040c42b433f14422dae2ab3703b
BHASKAR58900 <115164251+BHASKAR58900@users.noreply.github.com>
2024-03-28 12:20:19 +0530

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,36 @@
+import random
+ 
+def no(random):
+    no = random.randint(1,6)
+    if no==1:   
+      print("[-----]")
+      print("[     ]")
+      print("[  0  ]")
+      print("[     ]")
+      print("[-----]")
+    if no==2:
+     print("[-----]")
+     print("[     ]")
+     print("[ 000 ]")
+     print("[     ]")
+     print("[-----]")
+    if no==1:
+      print("[-----]")
+      print("[0   0]")
+      print("[  0  ]")
+      print("[0   0]")
+      print("[-----]")
+    if no==1:
+       print("[-----]")
+       print("[0   0]")
+       print("[     ]")
+       print("[0   0]")
+       print("[-----]")
+
+no=input("enter no ")
+no(int(random))
+
+
+
+
+
